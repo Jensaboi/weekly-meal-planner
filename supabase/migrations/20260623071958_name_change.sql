9b6a33947e6124e8d6c03ca3ad6fed500b4809ec
Jensaboi <jens.lundin1005@gmail.com>
2026-06-23 09:22:19 +0200

    Refactor: Edited last commit, to have the correct name for the view.

---
+++ supabase/migrations/20260623071958_name_change.sql
@@ -0,0 +1,54 @@
+DROP VIEW IF EXISTS recipe_details;
+
+DROP VIEW IF EXISTS recipe_catalog;
+
+CREATE VIEW recipe_details AS
+SELECT
+  r.*,
+
+  -- ingredients
+  (
+    SELECT json_agg( to_jsonb(i))
+    FROM recipe_ingredients i
+    WHERE i.recipe_id = r.id
+  ) AS ingredients,
+
+  -- instructions
+  (
+    SELECT json_agg(to_jsonb(ins) ORDER BY ins.step)
+    FROM recipe_instructions ins
+    WHERE ins.recipe_id = r.id
+  ) AS instructions,
+
+  -- avg rating
+  (
+    SELECT AVG(rr.rating)
+    FROM recipe_reviews rr
+    WHERE rr.recipe_id = r.id
+  ) AS avg_rating,
+
+  -- total reviews
+  (
+    SELECT COUNT(*)
+    FROM recipe_reviews rr
+    WHERE rr.recipe_id = r.id
+  ) AS total_reviews,
+
+  -- categories
+  -- categories
+  (
+    SELECT json_agg(to_jsonb(rc))
+    FROM recipe_categories rc
+    JOIN recipe_categories_map rcm
+      ON rcm.category_id = rc.id
+    WHERE rcm.recipe_id = r.id
+  ) AS categories,
+
+  --images
+  (
+    SELECT json_agg(to_jsonb(rimg) ORDER BY rimg.is_default DESC)
+    FROM recipe_images rimg
+    WHERE rimg.recipe_id = r.id
+  ) AS images
+
+FROM recipes r;
