c412fc4428704a9145d0e0b1fb582a583831dfa7
Jens <jens.lundin1005@gmail.com>
2026-06-25 10:55:20 +0200

    Create create recipe route.

---
+++ app/(auth-required)/recipes/create/page.tsx
@@ -0,0 +1,61 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Link from "next/link";
+
+export default function CreateRecipePage() {
+  return (
+    <section className="container mx-auto px-4 lg:px-8 py-16">
+      <h1 className="text-3xl font-bold">Create recipe</h1>
+
+      <p>Create a new recipe or import a existing one.</p>
+
+      <div className="grid gap-4 md:grid-cols-2 md:gap-16 py-16">
+        <Card>
+          <CardHeader>
+            <CardTitle className="text-xl font-semibold">
+              Create new recipe
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <CardDescription>Create a new recipe from scratch</CardDescription>
+          </CardContent>
+          <CardFooter>
+            <CardAction>
+              <Button size={"lg"}>
+                <Link href={"/recipes/create/new"}>Create new recipe</Link>
+              </Button>
+            </CardAction>
+          </CardFooter>
+        </Card>
+
+        <Card>
+          <CardHeader>
+            <CardTitle className="text-xl font-semibold">
+              Import existing recipe
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <CardDescription>
+              Import a recipe from an existing website.
+            </CardDescription>
+          </CardContent>
+          <CardFooter>
+            <CardAction>
+              <Button size={"lg"}>
+                <Link href={"/recipes/create/import"}>Import recipe</Link>
+              </Button>
+            </CardAction>
+          </CardFooter>
+        </Card>
+      </div>
+    </section>
+  );
+}
--- features/recipe/recipe.schema.ts
+++ features/recipe/recipe.schema.ts
@@ -6,3 +6,11 @@ export const NewReview = z.object({
   rating: z.coerce.number("You must leave a review rating."),
   recipe_id: z.coerce.number("Invalid recipe id."),
 });
+
+export const NewIngredient = z.object({});
+
+export const NewInstruction = z.object({});
+
+export const NewCategory = z.object({});
+
+export const NewRecipe = z.object({});
