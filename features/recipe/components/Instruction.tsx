8304bc9d7870ddc17dfae639b8c7683deef47969
Jens <jens.lundin1005@gmail.com>
2026-06-25 10:35:08 +0200

    Recipe details styles.

--- features/recipe/components/Instruction.tsx
+++ features/recipe/components/Instruction.tsx
@@ -2,19 +2,29 @@
 
 import { useState } from "react";
 import { InstructionData } from "../recipe.types";
-import { Item, ItemDescription, ItemTitle } from "@/components/ui/item";
+import { Item, ItemDescription } from "@/components/ui/item";
+import { Check } from "lucide-react";
+import clsx from "clsx";
 
 export default function Instruction({
   instruction,
 }: {
   instruction: InstructionData;
 }) {
-  const [isChecked, setIsChecked] = useState();
+  const [isChecked, setIsChecked] = useState(false);
   return (
     <Item variant={isChecked ? "muted" : "outline"} asChild>
-      <li>
-        <ItemTitle>{instruction.step}</ItemTitle>
-        <ItemDescription>{instruction.description}</ItemDescription>
+      <li className="grid grid-cols-[20px_1fr]">
+        <button
+          onClick={() => setIsChecked(prev => !prev)}
+          className="size-4 border border-border hover:cursor-pointer flex items-center justify-center"
+        >
+          {isChecked && <Check />}
+        </button>
+
+        <ItemDescription className={clsx(isChecked && "line-through")}>
+          <strong>{instruction.step}.</strong> {instruction.description}
+        </ItemDescription>
       </li>
     </Item>
   );
